3e4d0fa01294509e9fe7ff5621a24cbde6663304
CarlosVasquez13 <carlos1305vasquez@gmai.com>
2025-04-03 00:30:39 -0600

    add Redis Cache resource to Terraform configuration

--- db.tf
+++ db.tf
@@ -14,3 +14,13 @@ resource "azurerm_mssql_database" "db" {
     sku_name = "S0"
     tags = var.tags
 }
+
+resource "azurerm_redis_cache" "redis" {
+    name = "redis-${var.resource_group_name}-${var.environment}"
+    location = var.location
+    resource_group_name = azurerm_resource_group.rg.name
+    capacity = 1
+    family = "C"
+    sku_name = "Basic"
+    tags = var.tags
+}
